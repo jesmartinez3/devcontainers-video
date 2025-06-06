8ead86b02e6e9f022529a964dd7290896c6f3282
Jesse Martinez <jzsky@yahoo.com>
2025-06-06 03:13:32 +0000

    Added newline after question

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 
 int main() {
   std::string name;
-  std::cout << "Hello! What's your name?";
+  std::cout << "Hello! What's your name?\n";
   std::getline(std::cin, name);
   std::cout << "Nice to meet you, " << name << "!" << std::endl;
   return 0;
